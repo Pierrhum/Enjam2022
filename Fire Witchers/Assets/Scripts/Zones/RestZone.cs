4afadd233983114db775177a5331995666187089
Pierrhum <34352456+Pierrhum@users.noreply.github.com>
2022-11-20 16:34:22 +0100

    Bug Fix

--- Fire Witchers/Assets/Scripts/Zones/RestZone.cs
+++ Fire Witchers/Assets/Scripts/Zones/RestZone.cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class RestZone : BaseZone
 {
     public int EnergyGain = 2;
     public GameObject Chaudron;
     public GameObject ActiveWitch;
+    public GameObject ActiveWitchEnergySprite;
+    public Image ActiveWitchEnergyFill;
     
     private void Awake()
     {
@@ -24,6 +27,7 @@ public class RestZone : BaseZone
             {
                 ActiveWitch.SetActive(true);
                 Witches[0].gameObject.SetActive(false);
+                ActiveWitchEnergyFill.fillAmount = (float)Witches[0].CurrentEnergy / Witches[0].MaxEnergy;
             }
             yield return new WaitForSeconds(1);
             Witches.ForEach(w => w.UpdateEnergy(EnergyGain));
@@ -36,5 +40,13 @@ public class RestZone : BaseZone
         ActiveWitch.SetActive(false);
         Witches[0].gameObject.SetActive(true);
         Witches.Clear();
+    }
+    public void OnHover(BaseEventData data)
+    {
+        ActiveWitchEnergySprite.SetActive(true);
+    }
+    public void OnExit(BaseEventData data)
+    {
+        ActiveWitchEnergySprite.SetActive(false);
     }
 }
